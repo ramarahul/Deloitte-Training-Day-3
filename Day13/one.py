1d2b16c000f682122e824b008c4a89bbcc6508bd
Venkata Rama Rahul Chintalapati <ramarahulchintalapati@gmail.com>
2020-01-08 17:05:03 +0530

    Add files via upload

---
+++ Day13/one.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def funca():
+    print("welcome function")
+
+type(funca)
+
+"""this is to
+demonstrate
+sample data"""
+
+
+# In[ ]:
+
+
+
+
---
+++ Day13/pramod.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def red():
+    print("blood")
+
+
+# In[2]:
+
+
+
+def yellow():
+    print("lemon")
+
+
+# In[3]:
+
+
+def orange():
+    red()
+    yellow()
+    print("the above makes orange")
+
+
+# In[4]:
+
+
+
+def blue():
+    print("wednesday blues")
+
+
+# In[9]:
+
+
+from amrit import green
+
+
+# In[6]:
+
+
+blue.__name__
+
+
+# In[7]:
+
+
+import this
+
+
+# In[10]:
+
+
+import importlib
+
+
+# In[13]:
+
+
+#importlib.reload(amrit)
+
+
+# In[14]:
+
+
+#import amrit
+
+
+# In[ ]:
+
+
+
+
